5bc97bafb68e530865b6b8587b71eedfd2ef238a
0505Keitan <0505Keitan@gmail.com>
2020-05-17 00:41:53 +0900

    update : Vundleのインストールを追加

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@
 
 ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
